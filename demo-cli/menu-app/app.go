4db28bdedf428a40ec37b9bb3817f57ab299cf0d
psx95 <pranav.ps95@hotmail.com>
2023-07-16 22:07:01 -0400

    Implement print menu & add menu items

--- demo-cli/menu-app/app.go
+++ demo-cli/menu-app/app.go
@@ -7,10 +7,23 @@ import (
 	"strings"
 )
 
+type MenuItem struct {
+	name   string
+	prices map[string]float64
+}
+
+// Initialize menu with some pre-filled entries
+var menu = []MenuItem{
+	{name: "Coffee", prices: map[string]float64{"venti": 5.45, "grande": 3.50, "tall": 2.20}},
+	{name: "Hot Chocolate", prices: map[string]float64{"large": 3.00, "medium": 2.50, "small": 2.00}},
+}
+
+var in *bufio.Reader
+
 func main() {
 	fmt.Println("Welcome to the Menu App!")
 	// create buffered reader to read user input
-	in := bufio.NewReader(os.Stdin)
+	in = bufio.NewReader(os.Stdin)
 	choice := "a" // initial choice invalid
 
 	for choice != "q" {
@@ -24,13 +37,37 @@ func main() {
 func processOptions(choice string) {
 	switch choice {
 	case "1":
-		fmt.Println("Will Print Menu")
+		printMenu()
 	case "2":
-		fmt.Println("Will Add Items")
+		addMenuItem()
 	case "q":
 		fmt.Println("Exiting Application")
 	default:
 		fmt.Println("Please enter a valid option")
+	}
+}
+
+func printMenu() {
+	for _, menuItem := range menu {
+		fmt.Println(menuItem.name)
+		fmt.Println(strings.Repeat("-", 20))
+		for size, price := range menuItem.prices {
+			fmt.Printf("%20s\t%.2f\n", size, price)
+		}
+	}
+}
+
+func addMenuItem() {
+	fmt.Println("Enter name of Menu Item")
+	newItemName, err := in.ReadString('\n')
+	if err == nil {
+		newItem := MenuItem{
+			name:   strings.TrimSpace(newItemName),
+			prices: map[string]float64{},
+		}
+		menu = append(menu, newItem)
+	} else {
+		fmt.Println("Error accpeting menu item name", err)
 	}
 }
 
