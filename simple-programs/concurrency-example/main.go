9365be4816853799ccef530a378bebf403856365
Pranav Sharma <sharmapranav@google.com>
2023-09-01 15:37:52 +0000

    Add sample code for channels

--- simple-programs/concurrency-example/main.go
+++ simple-programs/concurrency-example/main.go
@@ -37,5 +37,27 @@ func demonstrateConcurrencyThroughWaitGroups() {
 }
 
 func demonstrateConcurrencyWithChannels() {
-	fmt.Println("Implement this")
+	var wg sync.WaitGroup
+	ch := make(chan string)
+	wg.Add(1)
+
+	go func() {
+		// send message to the channel
+		ch <- "Sent Message"
+		// No done here since we do not care when this message is sent
+		// as long as we are waiting on the task that is listening to
+		// this channel.
+	}()
+
+	// This is the async work that we care about and need to wait for
+	go func() {
+		// recieve the message from the channel
+		msg := <-ch
+		fmt.Printf("Received: %s\n", msg)
+		// Once we have recieved the message, we can call done so that
+		// the wait block is removed.
+		wg.Done()
+	}()
+	// wait till we have received the message from the channel
+	wg.Wait()
 }
