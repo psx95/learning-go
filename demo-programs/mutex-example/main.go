7ee86e244d1359ece1d24dad8119378e64741e30
psx95 <pranav.ps95@hotmail.com>
2023-10-05 01:10:25 -0400

    Add sample code to demonstrate mutex

---
+++ demo-programs/mutex-example/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	s := []int{}
+	var wg sync.WaitGroup
+	var m sync.Mutex
+
+	const iterations = 1000
+	wg.Add(iterations)
+
+	for i := 1; i <= iterations; i++ {
+		go func() {
+			m.Lock()
+			defer m.Unlock()
+			// if we did not acquire lock in this goroutine,
+			// ocassionally multiple goroutines could run simultaneously and
+			// therefore access the memory of 's' at the same time, when that
+			// happens, only one of the operation would take effect, since both
+			// goroutines would access the same value of 's' and overwrite each
+			// other.
+			s = append(s, 1)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(len(s))
+}
